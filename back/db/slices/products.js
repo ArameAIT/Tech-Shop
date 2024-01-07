0b498c8bada2ba72d2f8f27015f637790ef84143
Arame Hovhannisyan <narhov123456@gmail.com>
2024-01-07 22:20:35 +0400

    add totalCount to get request

--- back/db/slices/products.js
+++ back/db/slices/products.js
@@ -8,27 +8,43 @@ export async function getProducts(page, pageSize, sortBy, sortOrder, filterName,
     LEFT JOIN photos ph ON p.id = ph.product_id
     WHERE 1=1
     `;
+
+    let countQuery = `
+    SELECT COUNT(*) AS total
+    FROM products p
+    LEFT JOIN ratings r ON p.id = r.product_id
+    LEFT JOIN photos ph ON p.id = ph.product_id
+    WHERE 1=1
+`;
     const params = []
 
     if (filterName) {
         query += ` AND p.name LIKE ?`;
+        countQuery += ` AND p.name LIKE ?`;
         params.push(`%${filterName}%`);
     }
 
     if (filterMinPrice !== undefined) {
         query += ` AND p.value >= ?`;
+        countQuery += ` AND p.value >= ?`;
         params.push(filterMinPrice);
     }
 
     if (filterMaxPrice !== undefined) {
         query += ` AND p.value <= ?`;
+        countQuery += ` AND p.value <= ?`;
         params.push(filterMaxPrice);
     }
 
     if (filterCategory) {
         query += ` AND p.category = ?`;
+        countQuery += ` AND p.category = ?`;
         params.push(filterCategory);
     }
+
+    const countResult = await pool.query(countQuery, params);
+    const totalCount = countResult[0][0].total;
+
 
     if (sortBy && sortOrder) {
         query += ` ORDER BY ${sortBy} ${sortOrder.toUpperCase()}`;
@@ -38,7 +54,10 @@ export async function getProducts(page, pageSize, sortBy, sortOrder, filterName,
     params.push((+page - 1) * +pageSize, +pageSize);
 
     const products = await pool.query(query, params);
-    return products[0];
+    return {
+        products : products[0],
+        totalCount : totalCount
+    }
 }
 
 export async function addProduct(name, description, value, count, category) {
@@ -54,6 +73,6 @@ export async function updateProduct(id, name, description, value, count, categor
         , [name, description, value, count, category, id])
 }
 
-export async function deleteProduct(id){
-    await pool.query(`DELETE FROM products WHERE id = ?`,[id])
+export async function deleteProduct(id) {
+    await pool.query(`DELETE FROM products WHERE id = ?`, [id])
 }
