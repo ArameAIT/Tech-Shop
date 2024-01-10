92892934525f55d46c2ffd6b84ac67fd3a4add79
Arame Hovhannisyan <narhov123456@gmail.com>
2024-01-10 19:06:05 +0400

    set up react

--- back/index.js
+++ back/index.js
@@ -3,11 +3,13 @@ import authRouter from "./routs/auth.js"
 import productRouter from "./routs/products.js"
 import commentsRouter from "./routs/comments.js"
 import basketRouter from "./routs/basket.js"
+import cors from "cors"
 
 const app = express()
 const PORT = 4000
 
 app.use(express.json())
+app.use(cors())
 
 app.use("/auth", authRouter)
 app.use("/product", productRouter)
